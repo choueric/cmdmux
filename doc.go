a2107aae9e389df9e94bed1fcea6b6d634b7c4c9
choueric <zhssmail@gmail.com>
2017-01-17 10:13:54 +0800

    update doc.go

--- doc.go
+++ doc.go
@@ -8,10 +8,6 @@
 //  	"github.com/choueric/cmdmux"
 //  )
 //
-//  type Options struct {
-//  	profile string
-//  }
-//
 //  func rootHandler(args []string, data interface{}) (int, error) {
 //  	fmt.Println("Usage: build")
 //  	fmt.Println("       build kernel")
@@ -24,26 +20,15 @@
 //  }
 //
 //  func buildKernelHandler(args []string, data interface{}) (int, error) {
-//  	options := data.(*Options)
-//  	if options.profile == "" {
-//  		fmt.Println("invoke 'build kernel'")
-//  	} else {
-//  		fmt.Printf("invoke 'build kernel' for %s", options.profile)
-//  	}
+//  	fmt.Println("invoke 'build kernel'")
 //  	return 2, nil
 //  }
 //
 //  func main() {
-//  	var options Options
-//
 //  	cmdmux.HandleFunc("/", rootHandler)
 //  	cmdmux.HandleFunc("/build", buildHandler)
 //  	cmdmux.HandleFunc("/build/kernel", buildKernelHandler)
 //
-//  	if flagSet, err := cmdmux.FlagSet("/build/kernel"); err == nil {
-//  		flagSet.StringVar(&options.profile, "p", "", "speicify profile")
-//  	}
-//
-//  	cmdmux.Execute(&options)
+//  	cmdmux.Execute(nil)
 //  }
 package cmdmux
